9b16616f737a17531e4deedf4862dc017f51474d
Aymen OULED SAHEL <ouled.sahel.aymen@gmail.com>
2022-02-19 16:24:47 +0100

    First Rabbit MQ Projet: Initial Commit
    Signed-off-by: Aymen OULED SAHEL <ouled.sahel.aymen@gmail.com>

---
+++ src/main/java/com/aos/rabbitmq/_first_rabbit_java_queue/App.java
@@ -0,0 +1,13 @@
+package com.aos.rabbitmq._first_rabbit_java_queue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
